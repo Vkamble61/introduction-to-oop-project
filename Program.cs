836830455805f3af921edbfd5b8abfe21ee5bf9a
Vkamble61 <varsha.kamble61@gmail.com>
2022-09-04 11:08:55 +0100

    Add files via upload
    OOP Exercise Complete

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace introduction_to_oop
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pigeon pippa = new Pigeon("Pippa", "Grey");
+            pippa.Speak();
+            pippa.Fly();
+            pippa.EatPizza();
+            pippa.Spin();
+            pippa.DoTheCaterpiller();
+            pippa.Jump();
+
+            Penguin pingu = new Penguin("Pingu", "Black & White");
+            pingu.Speak();
+            pingu.Fly();
+            pingu.Spin();
+            pingu.DoTheCaterpiller();
+            pingu.Jump();
+
+
+            Parrot par = new Parrot("Pari", "Green");
+            par.Speak();
+            par.Fly();
+            par.DoTheCaterpiller();
+        }
+    }
+}
