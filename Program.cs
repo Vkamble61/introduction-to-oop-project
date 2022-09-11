f0ae11f1473e375bfba4222318b263b6a0cf96b4
VKamble61 <varsha.kamble61@gmail.com>
2022-09-11 11:21:43 +0100

    doulble fly in Parrot

--- Program.cs
+++ Program.cs
@@ -1,24 +1,22 @@
 ﻿using introduction_to_oop;
-{
 
-    var pippa = new Pigeon("Pippa", "Grey");
-    pippa.Speak();
-    pippa.Fly();
-    pippa.EatPizza();
-    pippa.Spin();
-    pippa.DoTheCaterpiller();
-    pippa.Jump();
+var pippa = new Pigeon("Pippa", "Grey");
+pippa.Speak();
+pippa.Fly();
+pippa.EatPizza();
+pippa.Spin();
+pippa.DoTheCaterpiller();
+pippa.Jump();
 
-    var pingu = new Penguin("Pingu", "Black & White");
-    pingu.Speak();
-    pingu.Fly();
-    pingu.Spin();
-    pingu.DoTheCaterpiller();
-    pingu.Jump();
+var pingu = new Penguin("Pingu", "Black & White");
+pingu.Speak();
+pingu.Fly();
+pingu.Spin();
+pingu.DoTheCaterpiller();
+pingu.Jump();
 
-    var par = new Parrot("Pari", "Green");
-    par.Speak();
-    par.Fly();
-    par.DoTheCaterpiller();
-
-}
+var par = new Parrot("Pari", "Green");
+par.Speak();
+par.Fly();
+par.Fly();
+par.DoTheCaterpiller();
